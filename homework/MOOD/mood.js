440ddd4f653b26bb0f2922d5a98e1c0c43d48d9e
pane2k <lorder.rar@gmail.com>
2025-07-05 19:12:33 +0300

    edit: code review edits

--- homework/MOOD/mood.js
+++ homework/MOOD/mood.js
@@ -6,72 +6,26 @@ function mood(colorsCount) {
     console.time("TestTime")
     console.log('цветов: ' + colorsCount);
 
-    // Премещены элементы на 1 в начало массива, убран пустой элемент ''  
-    const colors = ['красный', 'оранжевый', 'жёлтый', 'зелёный', 
-                    'голубой', 'синий', 'фиолетовый'];
-    const ARR_LEN = colors.length // Длинна массива
+    const colors = ['','красный', 'оранжевый', 'жёлтый', 
+            'зелёный', 'голубой', 'синий', 'фиолетовый'];
+    const ARR_LEN = colors.length 
+    
+    let used = {}
 
-    try {
-        const MAX_TIME_TO_CALC = 1000 // ms на выполнение
-        const START_TIME = Date.now() // Время начала выполнения 
-
-        let used = {} // Созданный объект для хранения значений True или False 
-
-        for (let i = 0; i < colorsCount; i++) {
-            
-            // Проверка на то что у нас количество ключей у объекта
-            //  меньше длинны массива со цветами
-            if (Object.keys(used).length === ARR_LEN) {
-                throw {
-                    name: "OutOfColorsError",
-                    message: "Цветов больше нет, больше не могу вывести иначе будет нарушение условий задачи"
-                }
-            }
-
-            // Медленный способ изза постоянного перезапуска итерации цикла
-            // const n = randomDiap(0, ARR_LEN - 1); 
-
-            // Более быстрый способ поиска нового числа, 
-            //  лучше вообще явно передавать что числа использовались 
-            //  Быстрее предыдущего примерно в 15~8 раз по тестам, без console.log 
-            //  (она слишком долго выполняется на массиве из 1000 элементов)
-            let n = null
-            do {
-                // Проверка по времени выполнения, нужно если 
-                //  пользователь случайно ввел один и тот же цвет два раза. 
-                //  Возникает изза проверки по размеру colors и проверки на присудствие цветов 
-                //  Для исправления можно изменить строку "used[colors[n]] = true" на "used[n] = true"
-                if (Date.now() - START_TIME > MAX_TIME_TO_CALC) {
-                throw {
-                    name: "TimeToCalculateError",
-                    message: `ИСКЛЮЧЕНИЕ: Выполнение фукции было больше чем ${MAX_TIME_TO_CALC} мс, наврядли вы ввели столько цветов, чтобы было так долго. Установите время больше если требуется`
-                    }
-                }
-                // Изменён вызов функции, чтобы можно было указывать 
-                //  сколько угодно цветов
-                n = randomDiap(0, ARR_LEN - 1);
-            } while (colors[n] in used)
-
-            used[colors[n]] = true // установка флага 
-            const colorName = colors[n]; // установка цвета
-            console.log(colorName);
+    for (let i = 0; i < colorsCount; i++) {
+        if (Object.keys(used).length === ARR_LEN - 1) {
+            console.warn("БОЛЬШЕ НЕТ ЦВЕТОВ!!!"); break
         }
+        const n = randomDiap(1, ARR_LEN - 1);
+        if (n in used) {
+            i--
+            continue
+        }
+        used[n] = true // установка флага 
+        const colorName = colors[n]; // установка цвета
+        console.log(colorName);
     }
-    catch (ex) {
-        if (ex.name === "TimeToCalculateError") {
-            // Исключение если слишком долго выполняется код
-            console.error(ex.name + ": " + ex.message)
-        }
-        else if (ex.name === "OutOfColorsError") {
-            // Исключение если больше нету цветов
-            console.warn(ex.name + ": " + ex.message)
-        }
-        else {
-            // Остаьные исключения
-            console.error(ex.name + ": " + ex.message)
-        }
-    }
-    console.timeEnd("TestTime")
+        
 }
 
-mood(1000);
+mood(3);
--- homework/vowels/main.js
+++ homework/vowels/main.js
@@ -1,37 +1,13 @@
 const str = "Ехал Грека через реку, видит Грека в реке рак, сунул Грека руку в реку, рак за руку Греку цап"
 
 function countVowels(str) {
-    
+    const letters = "уеёыаоэяию"
+    const LETTERS_SET = new Set((letters + letters.toUpperCase()).split(""));
 
-    // == МЕТОД ПОЛУЧЕНИЯ ЦИФРОВОГО ЗНАЧЕНИЯ БУКВ В UTF-8 == 
-    // const letters = "уеёыаоэяию" // гласные буквы
-    // let intLetters = []
-    // for (let i = 0; i < letters.length; i++) {
-    //     intLetters[i] = Number(letters.charCodeAt(i)); // получение символа
-    //     intLetters[i+letters.length] = Number(letters.charCodeAt(i)-32) // смещение на 32 символа для получения больших букв 
-    // }
-
-    // == ГОТОВЫЙ МАССИВ ЦИФРОВЫХ ЗНАЧЕНИЙ ДЛЯ ОПТИМИЗАЦИИ ==
-    // const intLetters = [
-    //     1091, 1077, 1105, 1099, 1072, 1086, 1101, 1103, 1080, 1102,
-    //     1059, 1045, 1073, 1067, 1040, 1054, 1069, 1071, 1048, 1070
-    // ]
-    
-
-    // Сделали из значений массива сет для сложности алгоритма O(n) - очень эффективный метод
-    const LETTERS_SET = new Set([
-        1091, 1077, 1105, 1099, 1072, 1086, 1101, 1103, 1080, 1102,
-        1059, 1045, 1073, 1067, 1040, 1054, 1069, 1071, 1048, 1070
-    ]);
     let howMuch = 0;
-    console.time("set");
     for (let i = 0; i < str.length; i++) {
-        // Проверяем есть ли в set нужное нам значение
-        if (LETTERS_SET.has(str.charCodeAt(i))) {
-            howMuch++;
-        }
+        LETTERS_SET.has(str[i]) ? howMuch++ : 0
     }
-    console.timeEnd("set");
     return howMuch;
 }
 
